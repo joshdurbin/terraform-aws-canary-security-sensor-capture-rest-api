961463ce59d072c042bdb0e43bf5cd3983a12392
Josh Durbin <joshua.durbin@simplytapp.com>
2017-03-25 10:17:52 -0700

    reverting assumed role and pushing changes with data for other policies

--- security_and_policies.tf
+++ security_and_policies.tf
@@ -1,23 +1,24 @@
-data "aws_iam_policy_document" "canary_sensor_api_rest_gateway_role_assumed_role_policy" {
-
-  statement {
-
-    actions = ["sts:AssumeRole"]
-    principals {
-
-      type = "service"
-      identifiers = ["apigateway.amazonaws.com"]
-    }
-  }
-}
-
 resource "aws_iam_role" "canary_sensor_api_rest_gateway_role" {
 
   name = "canary_sensor_api_rest_gateway_role"
-  assume_role_policy = "${data.aws_iam_policy_document.canary_sensor_api_rest_gateway_role_assumed_role_policy.json}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-data "aws_iam_policy_document" "dynamo_db_access_rest_gateway_document" {
+data "aws_iam_policy_document" "dynamo_db_access_rest_gateway" {
 
   statement {
 
@@ -35,7 +36,7 @@ resource "aws_iam_role_policy" "dynamo_db_access_rest_gateway" {
 
   name = "dynamo_db_access_rest_gateway"
   role = "${aws_iam_role.canary_sensor_api_rest_gateway_role.id}"
-  policy = "${data.aws_iam_policy_document.dynamo_db_access_rest_gateway_document.json}"
+  policy = "${data.aws_iam_policy_document.dynamo_db_access_rest_gateway.json}"
 }
 
 resource "aws_iam_role" "canary_sensor_api_capture_role" {
@@ -58,76 +59,81 @@ resource "aws_iam_role" "canary_sensor_api_capture_role" {
 EOF
 }
 
+data "aws_iam_policy_document" "dynamo_db_access" {
+
+  statement {
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:Scan",
+      "dynamodb:PutItem"
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.canary_sensor_data.arn}",
+      "${aws_dynamodb_table.canary_meta_information.arn}"
+    ]
+  }
+}
+
 resource "aws_iam_role_policy" "dynamo_db_access" {
 
   name = "canary_sensor_api_capture_dynamo_db_access"
   role = "${aws_iam_role.canary_sensor_api_capture_role.id}"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:GetItem",
-                "dynamodb:Scan",
-                "dynamodb:PutItem"
-            ],
-            "Resource": [
-                "${aws_dynamodb_table.canary_sensor_data.arn}",
-                "${aws_dynamodb_table.canary_meta_information.arn}"
-            ]
-        }
-    ]
+  policy = "${data.aws_iam_policy_document.dynamo_db_access.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "kms_access" {
+
+  statement {
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt"
+    ]
+
+    resources = [
+      "${var.kms_arn}"
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "kms_access" {
 
   name = "canary_sensor_api_capture_kms_access"
   role = "${aws_iam_role.canary_sensor_api_capture_role.id}"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "kms:Encrypt",
-                "kms:Decrypt"
-            ],
-            "Resource": "${var.kms_arn}"
-        }
-    ]
+  policy = "${data.aws_iam_policy_document.kms_access.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "log_access" {
+
+  statement {
+
+    actions = [
+      "logs:CreateLogGroup"
+    ]
+
+    resources = [
+      "arn:aws:logs:us-west-2:${data.aws_caller_identity.current_identify.account_id}:*"
+    ]
+  }
+
+  statement {
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "arn:aws:logs:us-west-2:${data.aws_caller_identity.current_identify.account_id}:log-group:/aws/lambda/canary_sensor_api_capture:*"
+    ]
+  }
 }
 
 resource "aws_iam_role_policy" "log_access" {
 
   name = "canary_sensor_api_capture_log_access"
   role = "${aws_iam_role.canary_sensor_api_capture_role.id}"
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "logs:CreateLogGroup",
-            "Resource": "arn:aws:logs:us-west-2:${data.aws_caller_identity.current_identify.account_id}:*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "arn:aws:logs:us-west-2:${data.aws_caller_identity.current_identify.account_id}:log-group:/aws/lambda/canary_sensor_api_capture:*"
-            ]
-        }
-    ]
-}
-EOF
+  policy = "${data.aws_iam_policy_document.log_access.json}"
 }
