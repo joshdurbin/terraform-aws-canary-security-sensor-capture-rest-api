22f0b3ca3df71808a944e381aa99185815f10af8
Josh Durbin <joshua.durbin@simplytapp.com>
2017-03-25 10:33:05 -0700

    policy doc changes test

--- security_and_policies.tf
+++ security_and_policies.tf
@@ -37,24 +37,22 @@ resource "aws_iam_role_policy" "dynamo_db_access_rest_gateway" {
   policy = "${data.aws_iam_policy_document.dynamo_db_access_rest_gateway.json}"
 }
 
+data "aws_iam_policy_document" "canary_sensor_api_capture_role" {
+
+  statement {
+    actions = [ "sts:AssumeRole" ]
+
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "canary_sensor_api_capture_role" {
 
   name = "canary_sensor_api_capture_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${data.aws_iam_policy_document.canary_sensor_api_capture_role.json}"
 }
 
 data "aws_iam_policy_document" "dynamo_db_access" {
