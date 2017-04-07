e0023d8b21aa9b0b569fb6826fd7b1633d51f128
Josh Durbin <joshua.durbin@simplytapp.com>
2017-04-07 00:34:55 -0500

    a little cleanup

--- canary_sensor_api_capture.py
+++ canary_sensor_api_capture.py
@@ -1,143 +1,148 @@
 from __future__ import print_function
 
 import logging
-import base64
-import boto3
+from base64 import b64decode, b64encode
 import datetime
-import os
-import json
-import cookielib
-import urllib2
+from os import environ
+from json import loads, dumps
+from cookielib import LWPCookieJar
+from urllib2 import Request, urlopen, HTTPError, HTTPHandler, HTTPSHandler, HTTPCookieProcessor, build_opener
 from string import Template
 from boto3.dynamodb.types import TypeDeserializer
+from boto3 import client
 
-kmsClient = boto3.client('kms')
-dynamodbClient = boto3.client('dynamodb')
+kmsClient = client('kms')
+dynamodbClient = client('dynamodb')
 
-USERNAME = os.environ['username']
-KMS_ARN = os.environ['kmsArn']
+USERNAME = environ['username']
+KMS_ARN = environ['kmsArn']
 
-ENCRYPTED_PASSWORD = os.environ['password']
-DECRYPTED_PASSWORD = kmsClient.decrypt(CiphertextBlob=base64.b64decode(ENCRYPTED_PASSWORD))['Plaintext']
+ENCRYPTED_PASSWORD = environ['password']
+DECRYPTED_PASSWORD = kmsClient.decrypt(CiphertextBlob=b64decode(ENCRYPTED_PASSWORD))['Plaintext']
 
 logger = logging.getLogger()
 
 def get_meta_information():
-
     meta_information = {}
 
     deserializer = TypeDeserializer()
 
-    getItemResult = dynamodbClient.get_item(TableName='canary_meta_information', Key={'tenantId':{'S':'default'}})
+    getItemResult = dynamodbClient.get_item(TableName='canary_meta_information', Key={'tenantId': {'S': 'default'}})
 
     if 'Item' not in getItemResult.keys():
-
         bearerToken = get_authentication_token(USERNAME, DECRYPTED_PASSWORD)
-        encryptedBearerToken = base64.b64encode(kmsClient.encrypt(KeyId=KMS_ARN,Plaintext=bearerToken)['CiphertextBlob'])
+        encryptedBearerToken = b64encode(
+            kmsClient.encrypt(KeyId=KMS_ARN, Plaintext=bearerToken)['CiphertextBlob'])
         devices = get_devices(bearerToken)
 
         dynamodbClient.put_item(TableName='canary_meta_information', Item={
-            'tenantId':{'S': 'default'},
-            'bearerToken':{'S': encryptedBearerToken},
-            'devices':{'SS': devices}
+            'tenantId': {'S': 'default'},
+            'bearerToken': {'S': encryptedBearerToken},
+            'devices': {'SS': devices}
         })
 
-    getItemResult = dynamodbClient.get_item(TableName='canary_meta_information', Key={'tenantId':{'S':'default'}})
+    getItemResult = dynamodbClient.get_item(TableName='canary_meta_information', Key={'tenantId': {'S': 'default'}})
 
     for key in getItemResult['Item'].keys():
         meta_information[key] = deserializer.deserialize(getItemResult['Item'][key])
 
-    meta_information['unencryptedBearerToken'] = kmsClient.decrypt(CiphertextBlob=base64.b64decode(meta_information['bearerToken']))['Plaintext']
+    meta_information['unencryptedBearerToken'] = \
+    kmsClient.decrypt(CiphertextBlob=b64decode(meta_information['bearerToken']))['Plaintext']
 
     return meta_information;
 
-def put_sensor_record(deviceId, sensorData):
 
+def put_sensor_record(deviceId, sensorData):
     dynamodbClient.put_item(TableName='canary_sensor_data', Item={
-        'deviceId':{'S': deviceId},
-        'time':{'S':datetime.datetime.now().isoformat()},
-        'temperature':{'N':sensorData['temperature']},
-        'humidity':{'N':sensorData['humidity']},
-        'air_quality':{'N':sensorData['air_quality']}
+        'deviceId': {'S': deviceId},
+        'time': {'S': datetime.datetime.now().isoformat()},
+        'temperature': {'N': sensorData['temperature']},
+        'humidity': {'N': sensorData['humidity']},
+        'air_quality': {'N': sensorData['air_quality']}
     })
 
-def get_devices(bearerToken):
 
+def get_devices(bearerToken):
     devices = []
 
     bearerAuthorizationTemplate = Template('Bearer $token')
 
-    locationsRequest = urllib2.Request('https://my.canary.is/api/locations')
+    locationsRequest = Request('https://my.canary.is/api/locations')
     locationsRequest.add_header('Authorization', bearerAuthorizationTemplate.substitute(token=bearerToken))
 
     try:
 
-        locationsResponse = urllib2.urlopen(locationsRequest)
-        locationsResponseAsJson = json.loads(locationsResponse.read())
+        locationsResponse = urlopen(locationsRequest)
+        locationsResponseAsJson = loads(locationsResponse.read())
 
         for location in locationsResponseAsJson:
             for device in location['devices']:
                 devices.append(str(device['id']))
 
-    except urllib2.HTTPError as e:
+    except HTTPError as e:
 
         if e.getcode() == 401:
 
-            errorMessageTemplate = Template('Received a $errorCode when attempting to get devices for account Dropping meta data which will refresh on next execution')
+            errorMessageTemplate = Template(
+                'Received a $errorCode when attempting to get devices for account Dropping meta data which will refresh on next execution')
 
             logger.error(errorMessageTemplate.substitute(errorCode=e.getcode()))
-            dynamodbClient.delete_item(TableName='canary_meta_information', Key={'tenantId':{'S':'default'}})
+            dynamodbClient.delete_item(TableName='canary_meta_information', Key={'tenantId': {'S': 'default'}})
         else:
 
-            errorMessageTemplate = Template('Received a $errorCode when attempting to get devices for account. The full response is: $errorResponse')
-            logger.error(errorMessageTemplate.substitute(errorCode=e.getcode(),errorResponse=e.read()))
-
+            errorMessageTemplate = Template(
+                'Received a $errorCode when attempting to get devices for account. The full response is: $errorResponse')
+            logger.error(errorMessageTemplate.substitute(errorCode=e.getcode(), errorResponse=e.read()))
 
     return devices
 
-def get_readings_for_device(deviceId, bearerToken):
 
+def get_readings_for_device(deviceId, bearerToken):
     compiledReadings = {}
 
     bearerAuthorizationTemplate = Template('Bearer $token')
     requestTemplate = Template('https://my.canary.is/api/readings?deviceId=$deviceId')
 
-    deviceReadingsRequest = urllib2.Request(requestTemplate.substitute(deviceId=deviceId))
+    deviceReadingsRequest = Request(requestTemplate.substitute(deviceId=deviceId))
     deviceReadingsRequest.add_header('Authorization', bearerAuthorizationTemplate.substitute(token=bearerToken))
 
     try:
 
-        deviceReadingsResponse = urllib2.urlopen(deviceReadingsRequest)
-        deviceReadingsResponseAsJson = json.loads(deviceReadingsResponse.read())
+        deviceReadingsResponse = urlopen(deviceReadingsRequest)
+        deviceReadingsResponseAsJson = loads(deviceReadingsResponse.read())
 
         for readings in deviceReadingsResponseAsJson:
             compiledReadings[readings['sensor_type']] = readings['value']
 
-    except urllib2.HTTPError as e:
+    except HTTPError as e:
 
         if e.getcode() == 401:
 
-            errorMessageTemplate = Template('Received a $errorCode when attempting to poll sensor readings for device $deviceId. Dropping meta data which will refresh on next execution')
+            errorMessageTemplate = Template(
+                'Received a $errorCode when attempting to poll sensor readings for device $deviceId. Dropping meta data which will refresh on next execution')
 
-            logger.error(errorMessageTemplate.substitute(deviceId=deviceId,errorCode=e.getcode()))
-            dynamodbClient.delete_item(TableName='canary_meta_information', Key={'tenantId':{'S':'default'}})
+            logger.error(errorMessageTemplate.substitute(deviceId=deviceId, errorCode=e.getcode()))
+            dynamodbClient.delete_item(TableName='canary_meta_information', Key={'tenantId': {'S': 'default'}})
         else:
 
-            errorMessageTemplate = Template('Received a $errorCode when attempting to poll sensor readings for device $deviceId. The full response is: $errorResponse')
-            logger.error(errorMessageTemplate.substitute(deviceId=deviceId,errorCode=e.getcode(),errorResponse=e.read()))
+            errorMessageTemplate = Template(
+                'Received a $errorCode when attempting to poll sensor readings for device $deviceId. The full response is: $errorResponse')
+            logger.error(
+                errorMessageTemplate.substitute(deviceId=deviceId, errorCode=e.getcode(), errorResponse=e.read()))
 
     return compiledReadings
 
-def get_authentication_token(username, password):
-    cookies = cookielib.LWPCookieJar()
-    handlers = [
-        urllib2.HTTPHandler(),
-        urllib2.HTTPSHandler(),
-        urllib2.HTTPCookieProcessor(cookies)
-    ]
-    opener = urllib2.build_opener(*handlers)
 
-    loginPageRequest = urllib2.Request('https://my.canary.is/login')
+def get_authentication_token(username, password):
+    cookies = LWPCookieJar()
+    handlers = [
+        HTTPHandler(),
+        HTTPSHandler(),
+        HTTPCookieProcessor(cookies)
+    ]
+    opener = build_opener(*handlers)
+
+    loginPageRequest = Request('https://my.canary.is/login')
     opener.open(loginPageRequest)
 
     for cookie in cookies:
@@ -149,20 +154,19 @@ def get_authentication_token(username, password):
         'password': password
     }
 
-    authenticationRequest = urllib2.Request('https://my.canary.is/api/auth/login', json.dumps(data))
+    authenticationRequest = Request('https://my.canary.is/api/auth/login', dumps(data))
     authenticationRequest.add_header('Content-Type', 'application/json')
     authenticationRequest.add_header('X-XSRF-TOKEN', crossSiteForgeryToken)
 
     authenticationResponse = opener.open(authenticationRequest)
     authenticationResponseBody = authenticationResponse.read()
-    authenticationResponseBodyAsJSON = json.loads(authenticationResponseBody)
+    authenticationResponseBodyAsJSON = loads(authenticationResponseBody)
 
     return authenticationResponseBodyAsJSON['access_token']
 
-def lambda_handler(event, context):
 
+def lambda_handler(event, context):
     meta = get_meta_information()
 
     for deviceId in meta['devices']:
-
         put_sensor_record(deviceId, get_readings_for_device(deviceId, meta['unencryptedBearerToken']))
