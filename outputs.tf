0a51678c5c80ec0bc70cc07307be03530170ace2
Josh Durbin <joshua.durbin@simplytapp.com>
2017-04-13 18:07:01 -0700

    adding outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "api_gateway_endpoint" {
 
   value = "https://${aws_api_gateway_deployment.canary_sensor_data_api_production_deployment.rest_api_id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_deployment.canary_sensor_data_api_production_deployment.stage_name}"
 }
+
+output "api_id" {
+
+  value = "${aws_api_gateway_rest_api.canary_sensor_data_api.id}"
+}
+
+output "api_stage_name" {
+
+  value = "${aws_api_gateway_deployment.canary_sensor_data_api_production_deployment.stage_name}"
+}
